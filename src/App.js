ffa499733f173c7f0fccd056f1f4b76c32ed8554
Przemyslaw Senderski <psenderski94@gmail.com>
2021-04-03 13:50:36 +0200

    Song and PLayer components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Song />
+      <Player />
+    </div>
+  );
 }
 
 export default App;
