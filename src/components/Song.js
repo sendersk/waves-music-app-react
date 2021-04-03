88e220b805e21bafafbd9a3182cf840a93a81fdf
Przemyslaw Senderski <psenderski94@gmail.com>
2021-04-03 13:49:17 +0200

    Song and Player component created, initial content added

---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <div>Picture</div>
+      <h1>Song Name</h1>
+      <div>Artist</div>
+    </div>
+  );
+};
+
+export default Song;
