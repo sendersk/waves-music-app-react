1c8144c2161628f011558379f6be6af00c46faa4
Przemyslaw Senderski <psenderski94@gmail.com>
2021-04-07 17:46:10 +0200

    useState for songs and currentSong added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
 import "./styles/app.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="app">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
