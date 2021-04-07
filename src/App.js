32ab1ccd843af65a55c23c93b3855c7fba028019
Przemyslaw Senderski <psenderski94@gmail.com>
2021-04-07 18:21:45 +0200

    isPlaying useState created

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import Song from "./components/Song";
 function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="app">
       <Song currentSong={currentSong} />
-      <Player />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
