90ce37c02b22fbc28148d3dab824e53bdc3609d1
Przemyslaw Senderski <psenderski94@gmail.com>
2021-04-09 18:54:28 +0200

    Library and LibrarySong components created

---
+++ src/components/Library.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Library = () => {
+  return (
+    <div className="library">
+      <h2>Library</h2>
+      <div className="library-songs"></div>
+    </div>
+  );
+};
+
+export default Library;
