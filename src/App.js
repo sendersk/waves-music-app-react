a54152fbbcd32be13cf0f138e07f0ef75d3db00b
Przemyslaw Senderski <psenderski94@gmail.com>
2021-04-15 21:54:38 +0200

    setSongs props added to Player

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ function App() {
         songInfo={songInfo}
         songs={songs}
         setCurrentSong={setCurrentSong}
+        setSongs={setSongs}
       />
       <Library
         audioRef={audioRef}
